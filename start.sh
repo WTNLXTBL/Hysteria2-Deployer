bed212dd9da02ccf5f1a632bec400d00d1df86a9
WTNLXTBL <FXD3D-J0001@outlook.com>
2025-05-24 16:10:24 +0800

    Optimized Code Format

--- start.sh
+++ start.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-#以下三行是需要填写的变量
-export HY2_PORT=''
-export HY2_PASSWORD=''
-export IP=''
-#此变量无需编辑
-WORKDIR="${HOME}/Hysteria2-Deployer"
 
+export HY2_PORT="" #端口
+export HY2_PASSWORD="" #密码
+export IP="" #服务器IP地址
+WORKDIR="`pwd`" #此变量无需编辑
+
+#生成自签SSL证书
 generate_cert() {
-echo "生成SSL证书中，一路回车即可"
-sleep 1
-rm -rf ${WORKDIR}/private.key ${WORKDIR}/csr.pem ${WORKDIR}/cert.crt
+echo "生成SSL证书中"
+rm -rf ${WORKDIR}/private.key ${WORKDIR}/csr.pem ${WORKDIR}/cert.crt 
 openssl genpkey -algorithm RSA -out ${WORKDIR}/private.key
 openssl req -new -key ${WORKDIR}/private.key -out ${WORKDIR}/csr.pem -subj "/C=/ST=/L=/O=/OU=/CN=/emailAddress="
 openssl req -x509 -days 3650 -key ${WORKDIR}/private.key -in ${WORKDIR}/csr.pem -out ${WORKDIR}/cert.crt
@@ -21,6 +20,7 @@ exit 1
 fi
 }
 
+#生成sing-box配置
 generate_config() {
 echo "生成sing-box配置中，文件为${WORKDIR}/config.json"
 sleep 1
@@ -29,7 +29,7 @@ cat > ${WORKDIR}/config.json << EOF
 {
   "log": {
     "disabled": false,
-    "level": "info",
+    "level": "warn",
     "timestamp": true
   },
   "inbounds": [{
@@ -38,7 +38,7 @@ cat > ${WORKDIR}/config.json << EOF
       "sniff_override_destination": true,
       "tag": "hy2",
       "listen": "::",
-      "listen_port": ${HY2_PORT} ,
+      "listen_port": "${HY2_PORT}",
       "up_mbps": 300,
       "down_mbps": 300,
       "users": [{
@@ -76,6 +76,7 @@ cat > ${WORKDIR}/config.json << EOF
   }
 }
 EOF
+sing-box check -c ${WORKDIR}/config.json
 if [ $? == 0 ]; then
 echo "完成"
 else
@@ -84,21 +85,21 @@ exit 1
 fi
 }
 
-export_list() {
-        rm -rf ${WORKDIR}/list
-        cat > ${WORKDIR}/list << EOF
+#打印hy2链接
+print_list() {
+echo "
 ----------------------------
 hy2配置：
 hysteria2://${HY2_PASSWORD}@${IP}:${HY2_PORT}/?insecure=1
 ----------------------------
-EOF
+"
 }
 
 main() {
-mkdir ${WORKDIR} 2>/dev/null
+cd ${WORKDIR}
 generate_cert
 [ ! -e ${WORKDIR}/config.json ] && generate_config
-export_list && cat ${WORKDIR}/list
+print_list
 nohup sing-box run -c ${WORKDIR}/config.json &
 }
 
